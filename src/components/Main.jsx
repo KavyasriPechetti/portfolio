8aff0c88430570dc6c778681667e17ba6746fc1b
KavyasriPechetti <kavyasripechetti@gmail.com>
2024-03-02 18:29:00 +0530

    aboutme

---
+++ src/components/Main.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Main() {
+    return (
+        <div>
+            <div className="nav1">
+                <div className="name"><h1>Kavya sri</h1></div>
+
+                <div><b>About</b></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <div><b>My Skills</b></div>  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <div> <b>Projects</b></div> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <div> <b>Contact Me</b>
+                </div>
+
+            </div>
+        </div>
+
+    )
+}
+
+export default Main;
